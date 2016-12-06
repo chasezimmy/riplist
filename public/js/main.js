174bdbbb4bb59c3fb62bde65392e3184ae9aec00
System Administrator <root@Chases-iMac.local>
2016-12-06 01:05:58 -0500

    favorite star gets unique id

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 function reply_(id) {
-	console.log(id);
+	//console.log(id);
 	concat = '#'.concat(id);
 	$(concat).addClass('fa-star').removeClass('fa-star-o');
 	$('#'.concat(id)).addClass('ripit');
+}
+
+
+function ajax_test(job_id) {
+	//console.log(id);
+    $.ajax({
+        url: '/add_favorite',
+        data: { id : job_id }
+    });
 }
